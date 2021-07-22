7356e60b565fa3f28166caa4dc7c9996fae99e06
Chun Fan <4866953+chunfantasy@users.noreply.github.com>
2021-07-22 20:56:47 +0200

    Add template

---
+++ Exercise5/src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="head">0722课后作业</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import FishTemplate from "./FishTemplate.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // EditorTemplate,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.head {
+  background-color: #5666f3;
+  color: #fff;
+  font-size: 30px;
+  height: 50px;
+  line-height: 50px;
+}
+
+.menu {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  .item {
+    display: flex;
+    border: 1px #e0e0e0 solid;
+    background: #f0f0f0;
+    width: 200px;
+    height: 100px;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    border-radius: 6px;
+  }
+}
+</style>
